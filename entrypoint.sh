9dc4250f1bc0e32d0bdfea81b40666ccc59387e6
OTSUKA, Yuanying <yuanying@fraction.jp>
2020-06-25 22:57:27 +0000

    Update entrypoint.sh to trap SIGTERM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# TODO: Use gosu to avoid executing `/manager` as root
-tgtd -f &> tgt.log &
+cleanup() {
+    echo "Cleanup tgtd..."
+    tgtadm --op update --mode sys --name State -v offline
+    tgt-admin --offline ALL
+    tgt-admin --update ALL -c /dev/null -f
+    tgtadm  --op delete --mode system
+}
+trap 'cleanup' SIGTERM
+
+tgtd -f &
 tgtadm --op update --mode sys --name State -v offline
 tgt-admin -e -c /etc/tgt/targets.conf
 tgtadm --op update --mode sys --name State -v ready
 
-exec "$@"
+wait $!
